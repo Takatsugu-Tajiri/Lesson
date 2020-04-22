e3fa0d7acd0d9076d2fde1452afcf2968e8f30af
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-04-22 13:19:42 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int  tuki = 4;
+		int hi = 22;
+		
+		System.out.println("今日は" + tuki +"月" +hi + "日" + "です。");
+		
+		Double P = 3.14;
+		System.out.println("円周率は" + P + "です。");
+	}
+
+}
