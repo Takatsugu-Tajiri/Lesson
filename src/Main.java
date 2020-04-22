52f25b295327c67ecd1fbd764cbfa1ff653b2db1
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-04-22 13:27:15 +0900

    ですを消した

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 		int  tuki = 4;
 		int hi = 22;
-		
-		System.out.println("今日は" + tuki +"月" +hi + "日" + "です。");
-		
+
+		System.out.println("今日は" + tuki +"月" +hi + "日" );
+
 		Double P = 3.14;
 		System.out.println("円周率は" + P + "です。");
 	}
