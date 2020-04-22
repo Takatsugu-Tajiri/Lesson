2bd47a79a75e9d6258cd760839dd1ede2356afb4
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-04-22 16:44:13 +0900

    変数の文字を変えました

--- src/KuKu.java
+++ src/KuKu.java
@@ -2,19 +2,19 @@
 public class KuKu {
 
 	public static void main(String[] args) {
-		int r=new java.util.Random().nextInt(9)+1;
-		int s=new java.util.Random().nextInt(9)+1;
-		
-		System.out.println(r + "×" + s +"=");
+		int num1=new java.util.Random().nextInt(9)+1;
+		int num2=new java.util.Random().nextInt(9)+1;
+		int num3= num1*num2;
+		System.out.println(num1 + "×" + num2 +"=");
 		
 		int input =new java.util.Scanner(System.in).nextInt();
 		
-		if (input==r*s) {
+		if (input==num3) {
 			System.out.println("正解");
 			
 		}
 		else {
-			System.out.println("間違い。正解は" + r*s);
+			System.out.println("間違い。正解は" + num3);
 		}
 
 
