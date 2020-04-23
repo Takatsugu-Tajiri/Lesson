d0f2d05d2bd19a680954ed9c0d39390779d78049
edu03 <edu03@EDUPC03.n00d01.kis.co.jp>
2020-04-23 14:49:54 +0900

    演習

---
+++ src/Hairetsu2.java
@@ -0,0 +1,42 @@
+
+public class Hairetsu2 {
+
+	public static void main(String[] args) {
+		int[] data = {5,8,7,9,6};
+			for( int d : data) {
+			System.out.println(d);
+			}
+		
+		
+		
+//		
+//		int[] prices = {100,200,150};
+//			
+//		
+//		for(int value : prices) {
+//			System.out.println(value);
+//		}
+//		
+//		String[] names = {"コーラ","コーヒー","水"};
+//		for (String n:names) {
+//			System.out.println(n);
+//		}
+//		
+//		
+//		int[] points = {80,90,60,70,50};
+//		int sum=0;
+//		for (int p: points){
+//			sum +=p;
+//			}
+//		System.out.println(sum);
+//	
+//	
+//	int count =0;
+//	for( int p   : points  ) {
+//		if (p>=70){
+//			count++;
+//		}
+//	
+//	}
+}
+}
